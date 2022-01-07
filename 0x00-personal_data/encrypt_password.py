8162d01985609ce28b4e16681cec0c087e94ec86
EugeneJoe <jmuthui70@gmail.com>
2022-01-07 12:48:15 +0000

    Update

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,6 +3,7 @@
 Defines a hash_password function to return a hashed password
 """
 import bcrypt
+from bcrypt import hashpw
 
 
 def hash_password(password: str) -> bytes:
@@ -12,7 +13,7 @@ def hash_password(password: str) -> bytes:
         password (str): password to be hashed
     """
     b = password.encode()
-    hashed = bcrypt.hashpw(b, bcrypt.gensalt())
+    hashed = hashpw(b, bcrypt.gensalt())
     return hashed
 
 
@@ -25,4 +26,4 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
     Return:
         bool
     """
-    return bcrypt.checkpw(password.encode(), hashed_password):
+    return bcrypt.checkpw(password.encode(), hashed_password)
