c92a630f0cb47b032f3db13a00f430d2f1b6babf
EugeneJoe <jmuthui70@gmail.com>
2022-01-20 11:18:51 +0000

    Updates

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -9,10 +9,7 @@ from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
-from user import (
-    Base,
-    User
-)
+from user import Base, User
 
 
 class DB:
