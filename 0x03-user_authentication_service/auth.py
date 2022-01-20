17dcfed0b3dbfd036ee456f6c9a7e89b8d6268fa
EugeneJoe <jmuthui70@gmail.com>
2022-01-20 11:44:30 +0000

    Fix pycodestyle error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,10 +27,11 @@ def _hash_password(password: str) -> bytes:
 
 
 def _generate_uuid() -> str:
-        """
-        Generate a uuid and return its string representation
-        """
-        return str(uuid4())
+    """
+    Generate a uuid and return its string representation
+    """
+    return str(uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
